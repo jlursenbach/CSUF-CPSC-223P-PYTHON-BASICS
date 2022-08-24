192eeba5920812ec23e742c86df80a8ae932cd63
Jacob Ursenbach <jacob.ursenbach@gmail.com>
2022-08-24 08:42:58 -0700

    Create main.py

---
+++ data/CPSC223P-01_lab_assignmentes/Assignment_1/main.py
@@ -0,0 +1,23 @@
+'''
+Name: Jacobu Ursenbach
+Date: 20220824
+File Purpose: Multiple Hello Functions
+'''
+
+from hello import *
+
+def main():
+    """
+    @Name: main
+    @Description:
+    @Param:
+    @Return: None
+    """
+
+    print("****** TITAN Hello Program ******")
+    print(helloworld())
+    print(helloname("Jacob"))
+
+
+if __name__ == "__main__":
+    main()
